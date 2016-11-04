3cafb12a0b24c06f35b8b508882d549ea7cd5605
Mark Ferris <littleferris@gmail.com>
2016-11-03 22:19:51 -0600

    added teal launcher

--- packages.sh
+++ packages.sh
@@ -2,3 +2,4 @@
 dpkg-deb -bZgzip ./projects/pixelsearch debs
 dpkg-deb -bZgzip ./projects/blackpixelsearch debs
 dpkg-deb -bZgzip ./projects/googlesearch debs
+dpkg-deb -bZgzip ./projects/tealpixelsearch debs
