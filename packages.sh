c9d128563eb7c83a83a4104c8d60c8c29e25c9a8
Mark Ferris <littleferris@gmail.com>
2016-11-13 19:16:23 -0700

    added squaresearch

--- packages.sh
+++ packages.sh
@@ -3,3 +3,4 @@ dpkg-deb -bZgzip ./projects/pixelsearch debs
 dpkg-deb -bZgzip ./projects/blackpixelsearch debs
 dpkg-deb -bZgzip ./projects/googlesearch debs
 dpkg-deb -bZgzip ./projects/tealpixelsearch debs
+dpkg-deb -bZgzip ./projects/squaresearch debs
